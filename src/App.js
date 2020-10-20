6c372637d3f787cef319aeaea83e78a0f9826c3b
mikandermickey <mikandermickey@outlook.com>
2020-10-20 10:49:17 +0200

    main layout added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MainLayout from "./components/MainLayout"
 
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Header />
+      <MainLayout />
     </>
 
   )
