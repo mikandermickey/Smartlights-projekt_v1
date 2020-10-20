ada0ded47db20b56d5893287f470a0860f2e3599
mikandermickey <mikandermickey@outlook.com>
2020-10-20 09:37:12 +0200

    headline

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Headline from './components/Headline';
+
+
+
+
+function App() {
+  return (
+    <header>
+      <Headline text="Control Panel" />
+    </header>
+
+  )
+};
+
+export default App;
---
+++ src/components/Headline.js
@@ -0,0 +1,10 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+const Headline = ({ text }) => {
+    return (
+        <h1>{text}</h1>
+    );
+}
+
+export default Headline;
